5bc160c9a231a5cbbeec25e5ab2fe3b0d456e420
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-09 18:02:02 +0530

    WPF-55585 - Sample added

---
+++ SchedulerWPF/SchedulerWPF/Model/JSONData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfScheduler
+{
+    public class JSONData
+    {
+        public string Subject { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// collecions for meetings.
+        /// </summary>
+        private ObservableCollection<Meeting> meetings;
+        public DateTime DateTime { get; set; }
+        public SchedulerViewModel()
+        {
+            jsonDataCollection = new List<JSONData>();
+            GetInformation();
+            DateTime = new DateTime(2017, 06, 01);
+        }
+        private async void GetInformation()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData");
+            jsonDataCollection = JsonConvert.DeserializeObject<List<JSONData>>(response);
+            this.Meetings = new ObservableCollection<Meeting>();
+            foreach (var data in jsonDataCollection)
+            {
+                Meetings.Add(new Meeting()
+                {
+                    EventName = data.Subject,
+                    From = Convert.ToDateTime(data.StartTime),
+                    To = Convert.ToDateTime(data.EndTime),
+                    color = Brushes.Red
+                }) ;
+            }
+        }
+        private List<JSONData> jsonDataCollection;
+        /// <summary>
+        /// Gets or sets meetings.
+        /// </summary>
+        public ObservableCollection<Meeting> Meetings
+        {
+            get
+            {
+                return this.meetings;
+            }
+            set
+            {
+                this.meetings = value;
+                this.RaiseOnPropertyChanged("Meetings");
+            }
+        }
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Invoke method when property changed.
+        /// </summary>
+        /// <param name="propertyName">property name</param>
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
